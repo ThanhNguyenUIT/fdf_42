f9f81a2e52509b6b50aa53364ec21de3d9c60dc8
ThanhNguyenUIT <thanhnt.uit@gmail.com>
2018-07-04 13:28:32 +0700

    modify devise pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
+    devise_parameter_sanitizer.permit :account_update, keys: %i(name phone address city)
     devise_parameter_sanitizer.permit :sign_up, keys: %i(name phone address city)
   end
 
